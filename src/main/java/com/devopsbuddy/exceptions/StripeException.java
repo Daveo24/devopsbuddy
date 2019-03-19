a4e088bb5fc04f35a0ca81a6138dd682a6cf1f5a
Dave Rowntree <davidrowntree@harvardtechnology.com>
2019-03-19 14:51:29 +0000

    exception handling

--- src/main/java/com/devopsbuddy/exceptions/StripeException.java
+++ src/main/java/com/devopsbuddy/exceptions/StripeException.java
@@ -1,5 +1,10 @@
 package com.devopsbuddy.exceptions;
 
+import com.stripe.exception.ApiException;
+
 public class StripeException extends RuntimeException {
 
+    public StripeException(Throwable e) {
+        super(e);
+    }
 }
